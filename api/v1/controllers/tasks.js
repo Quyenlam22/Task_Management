f4e6cee57238de3a78c38304399b4c6d6b9f5869
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-01-21 23:26:43 +0700

    Find status and Sort

--- api/v1/controllers/tasks.js
+++ api/v1/controllers/tasks.js
@@ -1,10 +1,23 @@
 const Task = require("../models/tasks.model")
 
 // [GET] /api/vi/tasks
-module.exports.index = async(req, res) => {
-    const tasks = await Task.find({
+module.exports.index = async (req, res) => {
+    let find = {
         deleted: false
-    })
+    }
+
+    //Find status
+    if (req.query.status) {
+        find.status = req.query.status
+    }
+
+    //Sort
+    let sort = {}
+    if (req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue
+    }
+
+    const tasks = await Task.find(find).sort(sort)
 
     res.json(tasks)
 }
@@ -17,7 +30,7 @@ module.exports.detail = async (req, res) => {
         }, {
             deleted: false
         }).select("title status timeStart timeFinish")
-    
+
         res.json(task)
     } catch (error) {
         res.json("Không tìm thấy!")
